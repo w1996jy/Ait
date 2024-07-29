dae3939c8129236136766fc55bddef0ee753dca2
anhuikylin <1264284076@qq.com>
2024-07-29 18:30:57 +0800

    ait

--- R/app_ui.R
+++ R/app_ui.R
@@ -15,15 +15,11 @@ app_ui <- function(request) {
                tabPanel("Merge File", merge_file_ui("merge_file")),
                tabPanel("Correlation Analysis", cor_ui("cor_id")),
                tabPanel("Descriptive Statistics", describe_ui("describe_id")),
-               tabPanel("Venn", Venn_ui("venn")),
-               tabPanel("DESeq2", deseq2_ui("deseq2_id")),
-               tabPanel("OPLS-DA", OPLS_DA_ui("data_import_raw_id")),
-               tabPanel("PCA", PCA_ui("data_import_tbl_id"))
+               tabPanel("DESeq2", deseq2_ui("deseq2_id"))
     ),
     navbarMenu("Plot",
                tabPanel("Bar", Bar_ui("bar")),
-               tabPanel("Point Plot", Point_ui("data_rm_noise_id")),
-               tabPanel("Line Plot", Line_ui("data_rm_outlier_id"))
+               tabPanel("Venn", Venn_ui("venn"))
     ),
     tabPanel("Help", help_ui("project_init_id"))
   )
